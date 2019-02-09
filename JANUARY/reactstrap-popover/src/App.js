9f9cf118c92a123b61fc647e92ade6cd164f6c08
JuanMa Garrido <juanma.garrido@gmail.com>
2019-02-09 13:36:07 +0100

    reacstrap popover

---
+++ JANUARY/reactstrap-popover/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Example from './components/Example'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Example />
+      </div>
+    );
+  }
+}
+
+export default App;
