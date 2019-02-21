0204e57b475a1b2ca0a6dc0054a5567bf1927e3f
JuanMa Garrido <juanma.garrido@gmail.com>
2019-02-21 13:10:36 +0100

    react-sticky-component

---
+++ FEBRUARY/react-sticky-component/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as App } from './App/'
+export { default as ExampleStuckContent } from './ExampleStuckContent/'
+export { default as PlaceholderP } from './PlaceholderP/'
+export { default as Sticky } from './Sticky/'
